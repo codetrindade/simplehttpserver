f780942369ecabb5da3e92422a63fcc3d70c018c
Trindade Code <trindadecode@gmail.com>
2023-07-26 18:32:25 +0100

    testing implementation of Args Logger

--- lib/src/commands/listen_command.dart
+++ lib/src/commands/listen_command.dart
@@ -1,13 +1,14 @@
 import 'dart:async';
 
+import 'package:args/args.dart';
 import 'package:args/command_runner.dart';
 import 'package:mason_logger/mason_logger.dart';
 import 'package:simplehttpserver/src/internal/httpserver/httpsserver.dart';
 import 'package:simplehttpserver/src/internal/httpserver/simplehttpserver.dart';
 
 class ListenCommand extends Command<int> {
-  ListenCommand({required Logger logger}) : _logger = logger;
-
+  ListenCommand({required Logger logger, required this.options}) : _logger = logger;
+  final ArgResults options;
   final Logger _logger;
   @override
   String get description => 'Listen Adress on 0.0.0.0:8080 [Address:Port]';
@@ -17,6 +18,7 @@ class ListenCommand extends Command<int> {
 
   FutureOr<int>? _startServer() async {
     var seconds = 0;
+    _logger.alert(options.arguments.toString());
     HttpsServer().verifyFiles();
     await SimpleHttpServer.run();
     await Future.doWhile(() async {
--- lib/src/internal/httpserver/simplehttpserver.dart
+++ lib/src/internal/httpserver/simplehttpserver.dart
@@ -6,6 +6,7 @@ import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_static/shelf_static.dart';
 
 
+
 //TODO default port will be check if is free
 
 class SimpleHttpServer{
@@ -19,7 +20,24 @@ class SimpleHttpServer{
   int get port => 8080;
 
   String get urlBase => 'http://$host:$port/';
+  
+  /// By default the following headers are in this set:
+  ///
+  ///     Content-Type: text/plain; charset=utf-8
+  ///     X-Frame-Options: SAMEORIGIN
+  ///     X-Content-Type-Options: nosniff
+  ///     X-XSS-Protection: 1; mode=block
+  ///
+  /// If the `Server` header is added here and the `serverHeader` is set as
+  /// well then the value of `serverHeader` takes precedence.
+  void setServerHeader(String header){
+    //TODO CHANGE TO ADD ALL NEW HEADERS
+    for (final newHeader in HttpHeaders.requestHeaders){
+      _server.serverHeader = newHeader;
+    }
+  }
 
+  
   static Future<SimpleHttpServer> run({
     String? path,
     int port = 8080,
@@ -33,7 +51,9 @@ class SimpleHttpServer{
     final pipeline = const Pipeline()
         .addMiddleware(logRequests())
         .addHandler(createStaticHandler(path, defaultDocument: 'index.html'));
+    
     final server = await io.serve(pipeline, address, port);
+  
     return SimpleHttpServer._(server, path);
   }
 }
