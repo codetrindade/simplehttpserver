5e9c71c92e87cefe81468d4052fd788a6c6b3108
Trindade Code <trindadecode@gmail.com>
2023-07-26 18:35:20 +0100

    New Banner

--- lib/src/internal/runner/banner.dart
+++ lib/src/internal/runner/banner.dart
@@ -1,18 +1,20 @@
-import 'package:simplehttpserver/src/internal/console/console.dart';
+import 'package:mason_logger/mason_logger.dart';
 
 const banner = '''
-   _____ _                 __     __  __________________                                
-  / ___/(_)___ ___  ____  / /__  / / / /_  __/_  __/ __ ________  ______   _____  _____
-  __ / / __ -__ / __ / / _ / /_/ / / /   / / / /_/ / ___/ _ / ___/ | / / _ / ___/
- ___/ / / / / / / / /_/ / /  __/ __  / / /   / / / ____(__  )  __/ /   | |/ /  __/ /    
-/____/_/_/ /_/ /_/ .___/_/___/_/ /_/ /_/   /_/ /_/   /____/___/_/    |___/___/_/     
-                /_/                                                       - v0.0.1
+╔═══╗          ╔╗     ╔╗ ╔╗╔════╗╔════╗╔═══╗╔═══╗                  
+║╔═╗║          ║║     ║║ ║║║╔╗╔╗║║╔╗╔╗║║╔═╗║║╔═╗║                  
+║╚══╗╔╗╔╗╔╗╔══╗║║ ╔══╗║╚═╝║╚╝║║╚╝╚╝║║╚╝║╚═╝║║╚══╗╔══╗╔═╗╔╗╔╗╔══╗╔═╗
+╚══╗║╠╣║╚╝║║╔╗║║║ ║╔╗║║╔═╗║  ║║    ║║  ║╔══╝╚══╗║║╔╗║║╔╝║╚╝║║╔╗║║╔╝
+║╚═╝║║║║║║║║╚╝║║╚╗║║═╣║║ ║║ ╔╝╚╗  ╔╝╚╗ ║║   ║╚═╝║║║═╣║║ ╚╗╔╝║║═╣║║ 
+╚═══╝╚╝╚╩╩╝║╔═╝╚═╝╚══╝╚╝ ╚╝ ╚══╝  ╚══╝ ╚╝   ╚═══╝╚══╝╚╝  ╚╝ ╚══╝╚╝ 
+           ║║                                       VERVISON: 0.0.1 BETA                                                
+           ╚╝                                                      
 ''';
 
 // Version is the current version
-const version = '0.0.1';
+const version = '0.0.1 BETA';
 
-// showBanner is used to show the banner to the user
+// ShowBanner is used to show the banner to the user
 void showBanner() {
-  write('${ansiSetColor(33)} $banner');
+  Logger().info(banner);
 }
